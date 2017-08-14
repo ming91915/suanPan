1dccb6dce5554f939e1f59a3c3ed57aa806674a8
Theodore <tlcfem@gmail.com>
2017-08-14 23:01:21 +1200

    COMMIT

--- Toolbox/elementParser.h
+++ Toolbox/elementParser.h
@@ -10,7 +10,6 @@ class Element;
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 void new_cp3_(unique_ptr<Element>&, istringstream&);
 void new_cp4_(unique_ptr<Element>&, istringstream&);
 void new_gq12_(unique_ptr<Element>&, istringstream&);
@@ -18,7 +17,6 @@ void new_ps_(unique_ptr<Element>&, istringstream&);
 void new_qe2_(unique_ptr<Element>&, istringstream&);
 void new_truss2d_(unique_ptr<Element>&, istringstream&);
 void new_c3d8_(unique_ptr<Element>&, istringstream&);
-
 #ifdef __cplusplus
 }
 #endif
