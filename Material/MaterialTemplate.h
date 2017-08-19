56302d21642262dc8ddc676dd4b82281d0317537
Theodore <tlcfem@gmail.com>
2017-08-19 16:51:34 +1200

    AMEND MATERIAL TEMPLATE

--- Material/MaterialTemplate.h
+++ Material/MaterialTemplate.h
@@ -2,20 +2,16 @@
 * @class MaterialTemplate
 * @brief A MaterialTemplate class.
 *
-* The Material class defines material models for FEM analyses.
+* The Material class defines material models for FEM analysis.
 *
 * The design pattern of this class is slightly different than the one of the Element
-* class. The main difference is that, the Material model needs to be copied for Element
-* local use while the Element obeject does not need to be duplicated.
+* class. The main one is that, the Material model needs to be copied for Element local use
+* while the Element object does not need to be duplicated.
 *
 * As suanPan uses smart pointers for dynamic memory management, the copy of a Material
 * prototype is defined as a unique pointer so that at Element level, there is no need to
 * handle the lifetime of the Material object. Although the `new` way is still possible,
-* there is a danger of memory leak.
-*
-* However, unique pointer does not support abstract class. Thus, the Material class is
-* defined as a concrete class. There is no pure virtual method defined. The drawback is
-* that the developer should find proper method to override manually.
+* there is a danger of memory leak like dangling pointers.
 *
 * The Material class itself defines a couple of member variables. Currently, strain,
 * stress, strain rate, general purpose history variables and stiffness quantities for both
@@ -38,7 +34,7 @@
 * Similar to other classes, there is a initialize method to allow the user to initialize
 * the object without creating a new object.
 * ```cpp
-* virtual void initialize();
+* virtual void initialize() = 0;
 * ```
 * This should be overridden in derived class to initialize the status variables. If your
 * Material model does not need to re-initialize, it is okay to place all initializations
@@ -46,12 +42,16 @@
 *
 * The method that is in charge of returning a copy is called
 * ```cpp
-* virtual unique_ptr<Material> getCopy();
+* virtual unique_ptr<Material> getCopy() = 0;
 * ```
 * By calling the copy constructor, this method can be implemented in a very concise way.
 * ```cpp
 * unique_ptr<Material> getCopy() { return make_unique<MaterialName>(*this); };
 * ```
+* According to the rule of three or the rule of five, since here the method calls the
+* default copy constructor of a derived class, if the derived class is so complicated that
+* the default constructor cannot handle, it is necessary to define the copy/assignment
+* (move) constructors and the destructor.
 *
 * The Material class defines four updating method.
 * ```cpp
@@ -66,9 +66,9 @@
 *
 * The other status control methods are also need to be implemented.
 * ```cpp
-* virtual int clearStatus();
-* virtual int commitStatus();
-* virtual int resetStatus();
+* virtual int clearStatus() = 0;
+* virtual int commitStatus() = 0;
+* virtual int resetStatus() = 0;
 * ```
 *
 * There is an additional method called
@@ -76,13 +76,11 @@
 * virtual double getParameter(const unsigned& = 0) const;
 * ```
 * This is universal method to return material properties based on the input argument, as
-* some Element objects may need some properties to compute particular data.
-*
-*
+* some Element objects may need some material properties to compute particular data.
 *
 * @author T
-* @date 10/08/2017
-* @version 0.1.0
+* @date 19/08/2017
+* @version 0.1.1
 * @file MaterialTemplate.h
 * @addtogroup Material
 * @{
