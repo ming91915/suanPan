7ede5309865e3fdd67ef8c95e5ac7f3e93e22c69
Theodore <tlcfem@gmail.com>
2017-11-04 18:55:14 +1300

    REMOVE EXTRA CODES

--- Material/Material2D/Bilinear2D.cpp
+++ Material/Material2D/Bilinear2D.cpp
@@ -20,16 +20,14 @@
 Bilinear2D::Bilinear2D(const unsigned& T, const double& E, const double& V, const double& Y, const double& H, const double& B, const PlaneType& M, const double& D)
     : Material2D(T, MT_BILINEAR2D, M, D)
     , base(0, E, V, Y, H, B, D) {
-    const auto EE = plane_type == PlaneType::S ? E : E / (1 - V * V);
-    const auto VV = plane_type == PlaneType::S ? V : V / (1 - V);
+    const auto EE = plane_type == PlaneType::S ? E : E / (1. - V * V);
+    const auto VV = plane_type == PlaneType::S ? V : V / (1. - V);
+
+    const auto t_factor = EE / (1. - VV * VV);
 
     initial_stiffness.zeros(3, 3);
-    initial_stiffness(0, 0) = 1;
-    initial_stiffness(1, 1) = 1;
-    initial_stiffness(2, 2) = (1. - VV) / 2.;
-    initial_stiffness(0, 1) = VV;
-    initial_stiffness(1, 0) = VV;
-    initial_stiffness *= EE / (1. - VV * VV);
+    initial_stiffness(0, 1) = initial_stiffness(1, 0) = VV * (initial_stiffness(0, 0) = initial_stiffness(1, 1) = t_factor);
+    initial_stiffness(2, 2) = .5 * t_factor * (1. - VV);
 }
 
 void Bilinear2D::initialize(const shared_ptr<DomainBase>&) {
